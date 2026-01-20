13537b80184710cfb8f7e17f4c3cb0016f254d49
chang <90279224+chang-07@users.noreply.github.com>
2026-01-20 18:26:16 -0500

    Refactor Navigation to Custom Floating NavBar and Add Core Feature Screens (#9)
    * started navbar
    
    * styled and created navbar
    
    * Prettify code
    
    * Navbar Fix, working routes to pages
    
    ---------
    
    Co-authored-by: saifnotsafe <saifalshawaf8@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- app/tabs/(tabs)/_layout.tsx
+++ app/tabs/(tabs)/_layout.tsx
@@ -1,38 +1,78 @@
 import React from 'react';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import {
+  Upload,
+  SquareActivity,
+  HeartPlus,
+  Calendar,
+  UserRound,
+} from 'lucide-react-native';
 import { Tabs } from 'expo-router';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import CustomNavBar from './customNavBar';
 
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
-  color: string;
-}) {
-  return <FontAwesome size={18} style={{ marginBottom: -3 }} {...props} />;
+function TabBarIcon({ color, Icon }: { color: string; Icon: any }) {
+  return <Icon color={color} size={22} />;
 }
 
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
-      }}
-    >
-      <Tabs.Screen
-        name="tab1"
-        options={{
-          title: 'Tab 1',
-          tabBarIcon: ({ color }) => <TabBarIcon name="star-o" color={color} />,
+    <>
+      <Tabs
+        screenOptions={{
+          // Disable the static render of the header on web
+          // to prevent a hydration error in React Navigation v6.
+          headerShown: useClientOnlyValue(false, true),
+          tabBarStyle: { display: 'none' },
         }}
-      />
-      <Tabs.Screen
-        name="tab2"
-        options={{
-          title: 'Tab 2',
-          tabBarIcon: ({ color }) => <TabBarIcon name="star-o" color={color} />,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="upload"
+          options={{
+            title: 'Upload',
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon color={color} Icon={Upload} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="services"
+          options={{
+            title: 'Services',
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon color={color} Icon={SquareActivity} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon color={color} Icon={HeartPlus} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="visits"
+          options={{
+            title: 'Visits',
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon color={color} Icon={Calendar} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon color={color} Icon={UserRound} />
+            ),
+          }}
+        />
+      </Tabs>
+
+      <CustomNavBar />
+    </>
   );
 }
