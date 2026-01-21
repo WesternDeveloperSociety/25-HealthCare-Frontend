f6750173b8acb3971910482db667534423f1fd78
nancyli15 <li.nancy.lian@gmail.com>
2026-01-20 19:49:56 -0500

    create upload modal component

--- app/tabs/(tabs)/upload.tsx
+++ app/tabs/(tabs)/upload.tsx
@@ -1,16 +1,18 @@
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Center } from '@/components/ui/center';
 import { Divider } from '@/components/ui/divider';
 import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
+import { DocumentUploadExample } from '@/components/documents/DocumentUploadExample';
 
 export default function Upload() {
   return (
     <Center className="flex-1 pb-32">
       <Heading className="font-bold text-2xl">Upload</Heading>
       <Divider className="my-[30px] w-[80%]" />
-      <Text className="p-4">Example below to use gluestack-ui components.</Text>
-      <EditScreenInfo path="app/tabs/(tabs)/upload.tsx" />
+      <Text className="p-4">Upload your documents using the component below.</Text>
+      
+      {/* Document Upload Component */}
+      <DocumentUploadExample />
     </Center>
   );
 }
